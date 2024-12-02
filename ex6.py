70be5bb47fa04e97648b1b1ce96f1059abe0f662
sharonelis <sharonelis1997@gmail.com>
2024-12-02 21:21:10 +0200

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,7 +1,11 @@
 #ex6
+# Write one-line function that accepts as an input a list of lists containing strings 
+# and returns a new list containing the number of palindrome strings in each sublist. 
+# Use nested filter / map / reduce functions. 
+
 from functools import reduce
 
-palindrome_count = lambda lst: list(map(lambda sublist: reduce(lambda acc, s: acc + 1 if s == s[::-1] else acc, sublist, 0), lst))
+palindrome_count = lambda lst: list(map(lambda sublist: reduce(lambda palindrome_counter, str: palindrome_counter + 1 if str == str[::-1] else palindrome_counter, sublist, 0), lst))
 
 #test6
 test_lists = [["madam", "test", "level"], ["abc", "racecar", "12321"], ["not", "a", "palindrome"]]
