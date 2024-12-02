04e3a29f8237f7c075724ad3a0e1f11eac6c0e96
sharonelis <sharonelis1997@gmail.com>
2024-12-02 21:20:01 +0200

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,5 +1,10 @@
 #ex2
-concat_strings = lambda lst: ' '.join([s for s in lst])
+# Write the shortest Python program, that accepts a list of strings and return a 
+# single string that is a concatenation of all strings with a space between them. 
+# Do not use the "join" function. Use lambda expressions. 
+
+from functools import reduce
+concat_strings = lambda lst: reduce(lambda x, y: x + ' ' + y, lst)
 
 #test2
 test_list = ["This", "is", "a", "test"]
