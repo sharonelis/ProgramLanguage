2c5ee0957401153fb7458d5f4a143ff7b439a49c
sharonelis <sharonelis1997@gmail.com>
2024-12-02 21:23:31 +0200

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,7 +1,12 @@
 #ex8
-is_prime = lambda x: x > 1 and all(x % i != 0 for i in range(2, int(x**0.5) + 1))
-prime_numbers_desc = lambda lst: sorted([x for x in lst if is_prime(x)], reverse=True)
+# Write a one-line Python function that takes a list of integers and returns a new list 
+# containing only the prime numbers, sorted in descending order. 
+# Use lambda expressions and list comprehensions.
+
+# Solution
+#                                                                                                 # x = a*b, a <= b <= sqrt(x)
+prime_numbers_desc = lambda lst: sorted([x for x in lst if x > 1 and all(x % i != 0 for i in range(2, int(x**0.5) + 1))], reverse=True)
 
 #test8
-test_numbers = [3, 12, 7, 11, 5, 16, 2]
+test_numbers = [3, 12, 7, 9, 11, 5, 16, 2]
 print(prime_numbers_desc(test_numbers))  # Output: [11, 7, 5, 3, 2]
